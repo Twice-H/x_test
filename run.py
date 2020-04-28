9f52831f1276997e24539acab87b37cdba37f6fa
Twice-H <sixseven@gmail.com>
2020-04-28 21:20:32 +0800

    "添加pip文件"

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from config import Conf
 
 
 
+
 if __name__ == "__main__":
     report_path = Conf.get_report_path() + os.sep + "result"
     report_html_path = Conf.get_report_path() + os.sep +"html"
