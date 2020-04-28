67c095590570138a1aae6c9cec3b26bb0f19e4d7
Twice-H <sixseven@gmail.com>
2020-04-28 21:18:11 +0800

    run

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import pytest
 from config import Conf
 
 
+
 if __name__ == "__main__":
     report_path = Conf.get_report_path() + os.sep + "result"
     report_html_path = Conf.get_report_path() + os.sep +"html"
